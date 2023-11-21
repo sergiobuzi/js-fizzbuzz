2c33bec82f8b8cd94c6ef02deacde0f6258bce7f
SergioBuzi <sergio.buzi97@gmail.com>
2023-11-21 15:55:54 +0100

    adding js starting point

---
+++ script.js
@@ -0,0 +1,24 @@
+// for (let i = 1; i <= 100; i++) {
+
+//         // numeri divisibili per 3 e per 5
+//     if (i % 3 === 0  &&  i % 5 === 0) {
+//         const fizzbuzz = "fizzbuzz";
+//         console.log("fizzbuzz");
+    
+//         // numeri divisibili per 3
+//     } else if (i % 3 === 0) {
+//         const fizz = "fizz";
+//         console.log("fizz");
+
+//         // numeri  divisibili per 5
+//     } else if (i % 5 ===0) {
+//         const buzz = "buzz";
+//         console.log("buzz");
+
+//         // tutti gli altri numeri
+//     } else {
+//         console.log(i);
+//     }
+
+// }
+
