b8aaba61d36197ec52778a9e8d2d54694362ec04
SergioBuzi <sergio.buzi97@gmail.com>
2023-11-21 16:30:42 +0100

    ex done

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
-// for (let i = 1; i <= 100; i++) {
+//creo le variabili e gli do la classe css
+const row = document.querySelector(".row");
+const container = document.querySelector(".container");
 
-//         // numeri divisibili per 3 e per 5
-//     if (i % 3 === 0  &&  i % 5 === 0) {
-//         const fizzbuzz = "fizzbuzz";
-//         console.log("fizzbuzz");
+
+//apro un ciclo che parte da 1 fino ad arrivare a 100
+for (let i = 1; i <= 100; i++) {
+
+    //creo un div generico
+    const square = document.createElement("div");
+    //aggiungo al div la classe square
+    square.classList.add("square");
+    //aggiungo il contenuto di square alla variabile row
+    row.append(square);
+    //aggiungo il contenuto di row all'interno di container
+    container.append(row);
+
+
+        // i numeri divisibili per 3 e per 5 vengono sostituiti da fizzbuzz
+    if (i % 3 === 0  &&  i % 5 === 0) {
+        const fizzbuzz = "fizzbuzz";
+        console.log("fizzbuzz");
+        square.append(fizzbuzz);
+        square.classList.add("red");
     
-//         // numeri divisibili per 3
-//     } else if (i % 3 === 0) {
-//         const fizz = "fizz";
-//         console.log("fizz");
+        // i numeri divisibili per 3 vengono sostituiti da fizz
+    } else if (i % 3 === 0) {
+        const fizz = "fizz";
+        console.log("fizz");
+        square.append(fizz);
+        square.classList.add("green");
 
-//         // numeri  divisibili per 5
-//     } else if (i % 5 ===0) {
-//         const buzz = "buzz";
-//         console.log("buzz");
+        // numeri  divisibili per 5  vengono sostituiti da buzz
+    } else if (i % 5 ===0) {
+        const buzz = "buzz";
+        console.log("buzz");
+        square.append(buzz);
+        square.classList.add("yellow");
 
-//         // tutti gli altri numeri
-//     } else {
-//         console.log(i);
-//     }
+        // tutti gli altri numeri rimangono tali
+    } else {
+        console.log(i);
+        square.append(i);
+    }
 
-// }
+}
 
